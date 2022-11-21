ef2a55459741d76f3468ee736ff4de44b75a46d3
dpmproton <118693116+dpmproton@users.noreply.github.com>
2022-11-21 15:24:23 -0500

    Create post.sh

---
+++ post.sh
@@ -0,0 +1,10 @@
+apk update
+apk upgrade
+apk add firefox
+apk add flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub org.onlyoffice.desktopeditors
+apk adg xdg-user-dirs-package
+xdg-user-dirs-update
+lbu commit
+
