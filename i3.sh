de3d6a430b273c2ec62cb50a34cd8509c378dbb7
dpmproton <118693116+dpmproton@users.noreply.github.com>
2022-11-21 15:12:45 -0500

    Update i3.sh

--- i3.sh
+++ i3.sh
@@ -4,11 +4,12 @@ setup-xorg-base xf86-video-amdgpu xfce4 xfce4-terminal lightdm-gtk-greeter xfce-
 apk add faenza-icon-theme 
 setup-devd udev
 apk add gvfs udisks2 ntfs-3g gvfs-fuse gvfs-smb
-apk add xfce4-pulseaudio-plugin pulseaudio  pulseaudio-alsa alsa-plugins-pulse alsa-utilis alsa-utilis-doc alsa-lib alsaconf pavucontrol paman
+apk add xfce4-pulseaudio-plugin pulseaudio  pulseaudio-alsa alsa-plugins-pulse alsa-utils alsa-utils-doc alsa-lib alsaconf pavucontrol
 lbu commit
 
-rc-service dbus start && \
-rc-update add dbus && \
-rc-service lightdm start && \
-rc-update add lightdm && \
-
+rc-service udev start
+rc-update udev dbus
+rc-service dbus start
+rc-update add dbus
+rc-service lightdm start
+rc-update add lightdm &&
