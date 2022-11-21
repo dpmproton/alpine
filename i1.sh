2eb8b91e2cb253306e3bf39795dee6bda9f72ad4
dpmproton <118693116+dpmproton@users.noreply.github.com>
2022-11-21 13:34:55 -0500

    Update i1.sh

--- i1.sh
+++ i1.sh
@@ -2,7 +2,9 @@ setup-xorg-base xf86-video-amdgpu
 apk add gnome
 apk add gnome-apps-core
 apk add gnome-apps-extra
-
+setup-devd udev
+lbu commit -v
+rc-service udev start
 rc-service gdm start
 rc-update add gdm
 apk add bash
