ab6e610239f0cd9c72f4c3a7e09d2c0a2eec9dcc
dpmproton <118693116+dpmproton@users.noreply.github.com>
2022-11-22 16:13:07 -0500

    Update post.sh

--- post.sh
+++ post.sh
@@ -1,6 +1,6 @@
-apk add geany ristretto firefox libreoffice libreoffice-lang-es fontconfig msttcorefonts-installer
-update-ms-fonts
-fc-cache -f
-apk clean cache
-lbu commit -v
+doas apk add geany ristretto firefox libreoffice libreoffice-lang-es fontconfig msttcorefonts-installer
+doas update-ms-fonts
+doas fc-cache -f
+doas apk clean cache
+doas lbu commit -v
 
