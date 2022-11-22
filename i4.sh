d1fd638a9ce52907bcf00694a316d214546016f2
dpmproton <118693116+dpmproton@users.noreply.github.com>
2022-11-22 10:48:22 -0500

    Update i4.sh

--- i4.sh
+++ i4.sh
@@ -1,7 +1,7 @@
 lbu include /home
 lbu include /var
 setup-xorg-base xfce4 xfce4-terminal lightdm-gtk-greeter xfce-polkit xfce4-screensaver consolekit2 dbus-x11 xfce4-panel xfce4-screenshooter xfce4-power-manager xfce4-taskmanager xfce4-xkb-plugin 
-apk add faenza-icon-theme geany ristretto gvfs udisks2 ntfs-3g gvfs-smb
+apk add faenza-icon-theme gvfs udisks2 ntfs-3g gvfs-smb
 setup-devd udev
 apk add xfce4-pulseaudio-plugin pulseaudio  pulseaudio-alsa alsa-plugins-pulse alsa-utils alsa-utils-doc alsa-lib alsaconf alsa-ucm-conf pavucontrol
 addgroup pinocho audio
