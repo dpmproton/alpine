aa8fa94b15f7e550f8335d1675deb3e1d992108f
dpmproton <118693116+dpmproton@users.noreply.github.com>
2022-11-22 18:35:36 -0500

    Update i4.sh

--- i4.sh
+++ i4.sh
@@ -1,5 +1,6 @@
 lbu include /home
 lbu include /var
+lbu include /home/pinocho/.mozilla/firefox/
 setup-xorg-base xfce4 xfce4-terminal lightdm-gtk-greeter xfce-polkit xfce4-screensaver consolekit2 dbus-x11 xfce4-panel xfce4-screenshooter xfce4-power-manager xfce4-taskmanager xfce4-xkb-plugin 
 apk add faenza-icon-theme gvfs udisks2 ntfs-3g gvfs-smb
 setup-devd udev
