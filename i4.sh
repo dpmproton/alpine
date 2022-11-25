21175a504800a7c6640cf8d3c43b1ee1fc1f3d28
dpmproton <118693116+dpmproton@users.noreply.github.com>
2022-11-25 15:27:24 -0500

    Update i4.sh

--- i4.sh
+++ i4.sh
@@ -2,7 +2,7 @@ lbu include /home
 lbu include /var
 lbu include /home/pinocho/.mozilla/firefox/
 setup-xorg-base xfce4 xfce4-terminal lightdm-gtk-greeter xfce-polkit xfce4-screensaver consolekit2 dbus-x11 xfce4-panel xfce4-screenshooter xfce4-power-manager xfce4-taskmanager xfce4-xkb-plugin 
-apk add faenza-icon-theme gvfs udisks2 ntfs-3g gvfs-smb
+apk add faenza-icon-theme gvfs udisks2 ntfs-3g gvfs-smb thunar-archive-plugin
 setup-devd udev
 apk add xfce4-pulseaudio-plugin pulseaudio  pulseaudio-alsa alsa-plugins-pulse alsa-utils alsa-utils-doc alsa-lib alsaconf alsa-ucm-conf pavucontrol
 addgroup pinocho audio
