6014a8866d16c24b26d400a238a31231c30bba99
dpmproton <118693116+dpmproton@users.noreply.github.com>
2022-11-25 15:35:32 -0500

    Update i4.sh

--- i4.sh
+++ i4.sh
@@ -1,6 +1,10 @@
 lbu include /home
 lbu include /var
 lbu include /home/pinocho/.mozilla/firefox/
+adduser piocho
+echo pinocho:usb | chpasswd
+adduser pinocho wheel
+echo "permit persist wheel" > /etc/doas.conf
 setup-xorg-base xfce4 xfce4-terminal lightdm-gtk-greeter xfce-polkit xfce4-screensaver consolekit2 dbus-x11 xfce4-panel xfce4-screenshooter xfce4-power-manager xfce4-taskmanager xfce4-xkb-plugin 
 apk add faenza-icon-theme gvfs udisks2 ntfs-3g gvfs-smb thunar-archive-plugin
 setup-devd udev
