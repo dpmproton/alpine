569f4a1757aa9b4630dd841ab95321ff71eea167
dpmproton <118693116+dpmproton@users.noreply.github.com>
2022-11-28 12:23:20 -0500

    Update posti.sh

--- posti.sh
+++ posti.sh
@@ -1,7 +1,7 @@
 doas apk add geany ristretto fontconfig msttcorefonts-installer font-opensans font-freefont ttf-droid
 doas apk add cups cups-libs cups-pdf cups-client cups-filters hplip
 doas /etc/init.d/cupsd start
-rc-update add cupsd boot
+doas rc-update add cupsd boot
 doas update-ms-fonts 
 doas fc-cache -f
 doas apk clean cache
